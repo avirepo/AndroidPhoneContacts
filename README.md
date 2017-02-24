89a4e85f5d1d09406f104cb7b81de7ede6f86e88
Vikas <vikas.goyal@quovantis.com>
2017-02-24 16:54:20 +0530

    Updated Readme.md

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # androidphonecontacts
-Android Phone contact allows you to fetch PhoneBook contacts using simple query builder. It have great performance with even fetching large contacts phone book. 
+
+Android Phone contact allows you to fetch PhoneBook contacts using simple query builder.
+It have great performance even with large number of contacts phone book.
 
 Add Maven repository in your project.gradle file.
-maven {
-        url  "http://dl.bintray.com/vikasgoyal/maven" 
+
+
+    repositories {
+        maven {
+            url 'https://dl.bintray.com/vikasgoyal/maven'
+        }
     }
+
+
+And add dependency with following lines
+
+        compile 'com.avi.android:contacts:1.0'
+
--- app/src/main/java/avi/avi/contacts/ContactsActivity.java
+++ app/src/main/java/avi/avi/contacts/ContactsActivity.java
@@ -1,14 +1,35 @@
 package avi.avi.contacts;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class ContactsActivity extends AppCompatActivity {
+import com.avi.contacts.ContactQuery;
+import com.avi.contacts.ContactsFetchManager;
+import com.avi.contacts.model.ContactInfo;
+import com.avi.contacts.model.ContactQueryType;
+
+import java.util.ArrayList;
+
+public class ContactsActivity extends AppCompatActivity implements ContactQuery.ContactsResponseListener<ArrayList<ContactInfo>> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+        //Fetch all contacts
+        ContactsFetchManager.getInstance().fetchContacts(new ContactQuery
+                .Builder(ContactQueryType.ALL_PHONE_EMAIL)
+                .withResponseListener(this)
+                .build(this));
+    }
+
+    /**
+     * Method will be fire when their is RESULT which need to
+     * be pass on the UI which implement for the type
+     * @param data Result which need to pass on UI
+     */
+    @Override
+    public void onContactsResponse(ArrayList<ContactInfo> data) {
 
     }
 }
